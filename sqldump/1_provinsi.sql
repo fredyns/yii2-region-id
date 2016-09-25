afa6d2c6dd8d6be8cea65b681c91ab5b16521182
Fredy Nurman Saleh <email@fredyns.net>
2016-09-25 20:43:48 +0700

    isi database wilayah & kodepos

---
+++ sqldump/1_provinsi.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4deb1+deb.cihar.com~xenial.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 25, 2016 at 08:36 PM
+-- Server version: 5.7.15-0ubuntu0.16.04.1
+-- PHP Version: 7.0.8-0ubuntu0.16.04.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `daerahIndonesia`
+--
+
+--
+-- Dumping data for table `drhidn_provinsi`
+--
+
+INSERT INTO `drhidn_provinsi` (`id`, `nomor`, `nama`, `singkatan`) VALUES
+(1, '11', 'Aceh', NULL),
+(2, '12', 'Sumatera Utara', NULL),
+(3, '13', 'Sumatera Barat', NULL),
+(4, '14', 'Riau', NULL),
+(5, '15', 'Jambi', NULL),
+(6, '16', 'Sumatera Selatan', NULL),
+(7, '17', 'Bengkulu', NULL),
+(8, '18', 'Lampung', NULL),
+(9, '19', 'Kepulauan Bangka Belitung', NULL),
+(10, '21', 'Kepulauan Riau', NULL),
+(11, '31', 'DKI Jakarta', NULL),
+(12, '32', 'Jawa Barat', NULL),
+(13, '33', 'Jawa Tengah', NULL),
+(14, '34', 'DI Yogyakarta', NULL),
+(15, '35', 'Jawa Timur', NULL),
+(16, '36', 'Banten', NULL),
+(17, '51', 'Bali', NULL),
+(18, '52', 'Nusa Tenggara Barat', NULL),
+(19, '53', 'Nusa Tenggara Timur', NULL),
+(20, '61', 'Kalimantan Barat', NULL),
+(21, '62', 'Kalimantan Tengah', NULL),
+(22, '63', 'Kalimantan Selatan', NULL),
+(23, '64', 'Kalimantan Timur', NULL),
+(24, '65', 'Kalimantan Utara', NULL),
+(25, '71', 'Sulawesi Utara', NULL),
+(26, '72', 'Sulawesi Tengah', NULL),
+(27, '73', 'Sulawesi Selatan', NULL),
+(28, '74', 'Sulawesi Tenggara', NULL),
+(29, '75', 'Gorontalo', NULL),
+(30, '76', 'Sulawesi Barat', NULL),
+(31, '81', 'Maluku', NULL),
+(32, '82', 'Maluku Utara', NULL),
+(33, '91', 'Papua Barat', NULL),
+(34, '92', 'Papua', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
