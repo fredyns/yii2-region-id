18069117ae8bdda53e8f0dcb3ad5213771e7caa5
Fredy Nurman Saleh <email@fredyns.net>
2017-01-12 16:44:16 +0700

    rules & behaviors

--- models/Provinsi.php
+++ models/Provinsi.php
@@ -3,7 +3,8 @@
 namespace fredyns\daerahIndonesia\models;
 
 use Yii;
-use \fredyns\daerahIndonesia\models\base\Provinsi as BaseProvinsi;
+use yii\helpers\ArrayHelper;
+use fredyns\daerahIndonesia\models\base\Provinsi as BaseProvinsi;
 
 /**
  * This is the model class for table "_provinsi".
@@ -11,4 +12,17 @@ use \fredyns\daerahIndonesia\models\base\Provinsi as BaseProvinsi;
 class Provinsi extends BaseProvinsi
 {
 
+    /**
+     * data untuk opsi formulir
+     *
+     * @return array
+     */
+    static function options()
+    {
+        $query = static::find()
+            ->orderBy(['nama' => SORT_ASC])
+            ->all();
+
+        return ArrayHelper::map($query, 'id', 'nama');
+    }
 }
