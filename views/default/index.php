54b6efd22820bb681b6626b558ba9157993c22d5
Fredy Nurman Saleh <email@fredyns.net>
2017-01-13 17:34:28 +0700

    form sample

--- views/default/index.php
+++ views/default/index.php
@@ -1,15 +1,164 @@
 <?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\helpers\StringHelper;
+use yii\bootstrap\ActiveForm;
+use dmstr\bootstrap\Tabs;
+use kartik\select2\Select2;
+use kartik\depdrop\DepDrop;
+use fredyns\daerahIndonesia\models\Provinsi;
+
+$model = new \fredyns\daerahIndonesia\models\Kodepos;
+$this->title = Yii::t('app', 'Daerah Indonesia');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Daerah Indonesia'), 'url' => ['/'.\Yii::$app->controller->module->id]];
 ?>
 <div class="daerahIndonesia-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+
+    <h1>Daerah Indonesia</h1>
+
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
+        Berisi data seluruh wilayah Indonesia sesuai Permendagri No 39 tahun 2015.
+
+        <?=
+        Html::ol(
+            [
+            Html::a('Provinsi', ['provinsi/index']),
+            Html::a('Kota', ['kota/index']),
+            Html::a('Kecamatan', ['kecamatan/index']),
+            Html::a('Kelurahan', ['kelurahan/index']),
+            Html::a('Kodepos', ['kodepos/index']),
+            ], [
+            'encode' => false,
+            ]
+        );
+        ?>
     </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+
+    <h3>Contoh Formulir</h3>
+
+    <div class="sample-form">
+
+        <?php
+        $form = ActiveForm::begin([
+                'id' => 'Sample',
+                'layout' => 'horizontal',
+                'enableClientValidation' => true,
+                'errorSummaryCssClass' => 'error-summary alert alert-error'
+                ]
+        );
+        ?>
+
+        <p>
+
+            <?=
+                $form
+                ->field($model, 'provinsi_id')
+                ->widget(Select2::classname(),
+                    [
+                    'data' => Provinsi::options(),
+                    'pluginOptions' =>
+                    [
+                        'placeholder' => 'Pilih atau ketik nama Provinsi',
+                        'multiple' => FALSE,
+                        'allowClear' => TRUE,
+                        'tags' => TRUE,
+                        'maximumInputLength' => 255, /* country name maxlength */
+                    ],
+            ]);
+            ?>
+
+            <?=
+                $form
+                ->field($model, 'kota_id')
+                ->widget(DepDrop::classname(),
+                    [
+                    'data' => [],
+                    'type' => DepDrop::TYPE_SELECT2,
+                    'select2Options' => [
+                        'pluginOptions' => [
+                            'multiple' => FALSE,
+                            'allowClear' => TRUE,
+                            'tags' => TRUE,
+                            'maximumInputLength' => 255,
+                        ],
+                    ],
+                    'pluginOptions' => [
+                        'initialize' => TRUE,
+                        'placeholder' => 'Pilih atau ketik nama Kota',
+                        'depends' => ['kodepos-provinsi_id'],
+                        'url' => Url::to([
+                            "kota/depdrop-options",
+                            'selected' => $model->kota_id,
+                        ]),
+                        'loadingText' => 'Memuat Kota ...',
+                    ],
+            ]);
+            ?>
+
+            <?=
+                $form
+                ->field($model, 'kecamatan_id')
+                ->widget(DepDrop::classname(),
+                    [
+                    'data' => [],
+                    'type' => DepDrop::TYPE_SELECT2,
+                    'select2Options' => [
+                        'pluginOptions' => [
+                            'multiple' => FALSE,
+                            'allowClear' => TRUE,
+                            'tags' => TRUE,
+                            'maximumInputLength' => 255,
+                        ],
+                    ],
+                    'pluginOptions' => [
+                        'initialize' => TRUE,
+                        'placeholder' => 'Pilih atau ketik nama Kecamatan',
+                        'depends' => ['kodepos-kota_id'],
+                        'url' => Url::to([
+                            "kecamatan/depdrop-options",
+                            'selected' => $model->kecamatan_id,
+                        ]),
+                        'loadingText' => 'Memuat Kecamatan ...',
+                    ],
+            ]);
+            ?>
+
+            <?=
+                $form
+                ->field($model, 'kelurahan_id')
+                ->widget(DepDrop::classname(),
+                    [
+                    'data' => [],
+                    'type' => DepDrop::TYPE_SELECT2,
+                    'select2Options' => [
+                        'pluginOptions' => [
+                            'multiple' => FALSE,
+                            'allowClear' => TRUE,
+                            'tags' => TRUE,
+                            'maximumInputLength' => 255,
+                        ],
+                    ],
+                    'pluginOptions' => [
+                        'initialize' => TRUE,
+                        'placeholder' => 'Pilih atau ketik nama Kelurahan',
+                        'depends' => ['kodepos-kecamatan_id'],
+                        'url' => Url::to([
+                            "kelurahan/depdrop-options",
+                            'selected' => $model->kelurahan_id,
+                        ]),
+                        'loadingText' => 'Memuat Kelurahan ...',
+                    ],
+            ]);
+            ?>
+
+        </p>
+
+        <i>*contoh penerapannya lihat pada formulir kodepos</i>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
+
+
 </div>
