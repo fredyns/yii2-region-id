4bc92a5a33709efb9e838c37075e50603b886f77
Fredy Nurman Saleh <email@fredyns.net>
2021-06-23 05:13:28 +0700

    import postcode

---
+++ migrations/m210622_215753_import_postcode.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace fredyns\region\migrations;
+
+use fredyns\region\models\Postcode;
+use yii\db\Migration;
+
+/**
+ * Class m210622_215753_import_postcode
+ */
+class m210622_215753_import_postcode extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $postcodeCount = Postcode::find()->count();
+        if ($postcodeCount > 0) {
+            echo "{$postcodeCount} postcode found. import skipped.\n";
+            return true;
+        }
+
+        $this->batchInsert(Postcode::tableName(), ['number', 'subdistrict_id'], __DIR__ . '/_postcode.php');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210622_215753_import_postcode cannot be reverted.\n";
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210622_215753_import_postcode cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
